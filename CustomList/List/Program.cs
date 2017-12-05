755337cbc7a6828fb2fb26f91e8fb1c0d2fa9cc0
Chez <chez.vang@gmail.com>
2017-12-05 11:55:05 -0600

    working increment, decrement, and loop methods

--- CustomList/List/Program.cs
+++ CustomList/List/Program.cs
@@ -10,6 +10,8 @@ namespace List
     {
         static void Main(string[] args)
         {
+            CustomList customlist = new CustomList();
+
         }
     }
 }
--- CustomList/ListTest/ListTester.cs
+++ CustomList/ListTest/ListTester.cs
@@ -8,15 +8,67 @@ namespace ListTest
     public class ListTester
     {
         [TestMethod]
-        public void CustomList_Increment_IncreaseValue()
+        public void CustomList_Increment_IncreaseListCount()
         {
             //Arrange
             CustomList customlist = new CustomList();
+            int listValue = 0;
+            int addValue = 1;
 
             //Act
+            listValue = listValue + addValue;
 
             //Assert
+            Assert.AreEqual(listValue, 1);
+        }
 
+        [TestMethod]
+        public void CustomList_Decrement_DecreaseListCount()
+        {
+            //Arrange
+            CustomList customList = new CustomList();
+            int listValue = 0;
+            int decreaseValue = -1;
+
+            //Act
+            listValue = listValue + decreaseValue;
+
+            //Assert
+            Assert.AreNotEqual(listValue, 1);
+        }
+
+        [TestMethod]
+        public void CustomList_IncreaseListCount_IncreaseOverallListValue()
+        {
+            //Arrange
+            CustomList customlist = new CustomList();
+            int listValue = Int32.MaxValue;
+
+            //Act
+            for (int i = Int32.MaxValue; i < listValue; i++)
+            {
+                listValue += Int32.MaxValue;
+            }
+
+            //Assert
+            Assert.AreEqual(listValue, Int32.MaxValue);
+        }
+
+        [TestMethod]
+        public void CustomList_DecreaseListCount_DecreaseOverallListValue()
+        {
+            //Arrange
+            CustomList customlist = new CustomList();
+            int listValue = Int32.MaxValue;
+
+            //Act
+            for (int i = Int32.MaxValue; i < listValue; i--)
+            {
+                listValue += Int32.MaxValue;
+            }
+
+            //Assert
+            Assert.AreEqual(listValue, Int32.MaxValue);
         }
     }
 }
