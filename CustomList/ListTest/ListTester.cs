f003379a2e0e31fc0caf8ec35c5f0dbb528820dd
Chez <chez.vang@gmail.com>
2017-12-05 09:18:58 -0600

    initial commit

---
+++ CustomList/ListTest/ListTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ListTest
+{
+    [TestClass]
+    public class ListTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
